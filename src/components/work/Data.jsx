46f19481ac9147bd4d2c146f0f5bc5e6521558c3
Monojit Mondal <mondalmonojit1696@gmail.com>
2023-02-28 22:27:49 +0530

    New work added

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,6 +1,7 @@
 import Bluemercury from "../../assets/works/Bluemercury.png";
 import Bontime from "../../assets/works/Bontime.png";
 import Ecarry from "../../assets/works/E-Carry.png";
+import Eve from "../../assets/works/Eve.png";
 import LeadIQ from "../../assets/works/LeadIQ.png";
 import Medease from "../../assets/works/Medease.png";
 
@@ -11,7 +12,7 @@ export const projectsData = [
     title: "E-CARRY",
     description:
       "This is a online shopping E-commerce website for phone cases, covers, lifestyle & personal accessories. A collaborative project built by a team of 4 developers, completed in 5 days.",
-    tech: "React | Redux | CSS | Chakra-UI",
+    tech: "React | Redux | JSON-Server | Chakra-UI",
     code: "https://github.com/ninja-mono1696/acidic-rhythm-139",
     demo: "https://e-carry.netlify.app/",
     category: "react",
@@ -22,13 +23,24 @@ export const projectsData = [
     title: "Medsease",
     description:
       "This is a online pharmacy and medical store website specially for medicines. A collaborative project built by a team of 5 developers, executed in 5 days.",
-    tech: "React | Redux | CSS | Chakra-UI",
+    tech: "React | Redux | JavaScript | Chakra-UI",
     code: "https://github.com/kshivang80/maddening-neck-4975",
     demo: "https://wonderful-lily-4b490a.netlify.app/",
     category: "react",
   },
   {
     id: 3,
+    image: Eve,
+    title: "EVE",
+    description:
+      "This is an E-Commerce website specially for mobiles, laptops, watches, etc. A collaborative project built by a team of 5 developers executed in 5 days.",
+    tech: "React | TypeScript | NodeJS | MongoDB",
+    code: "https://github.com/Rkarthik25/rooted-scale-5183",
+    demo: "https://eve-india.netlify.app/",
+    category: "mern",
+  },
+  {
+    id: 4,
     image: Bluemercury,
     title: "Bluemercury",
     description:
@@ -39,7 +51,7 @@ export const projectsData = [
     category: "javascript",
   },
   {
-    id: 4,
+    id: 5,
     image: Bontime,
     title: "Bontime",
     description:
@@ -50,7 +62,7 @@ export const projectsData = [
     category: "mern",
   },
   {
-    id: 5,
+    id: 6,
     image: LeadIQ,
     title: "LeadIQ",
     description:
@@ -59,7 +71,7 @@ export const projectsData = [
     code: "https://github.com/ninja-mono1696/leadiQ_Clone",
     demo: "https://leadiq.netlify.app/",
     category: "react",
-  }
+  },
 ];
 
 export const projectNav = [
